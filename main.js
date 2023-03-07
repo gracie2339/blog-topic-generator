747c3ad5a6d00c644cbd0e7882bb5219c92c18a4
Grace Guste <guste.grace@gmail.com>
2023-03-07 14:35:41 -0500

    Added more templates

--- main.js
+++ main.js
@@ -16,7 +16,11 @@ let keywords = [
     'Tableau',
     'Analytics Engineering'
 ];
-
+let problems = [
+    'Duplicate Sessions in GA4 BigQuery',
+    "Can't Find Unique Session Key in BigQuery GA4",
+    "Conversions Not Adding Up Correctly in BigQuery GA4"
+];
 
 // Initialize variables
 let random_number = (Math.floor(Math.random() * 21) + 1);
@@ -24,14 +28,17 @@ let random_process_index = Math.floor(Math.random() * processes.length);
 let random_process = processes[random_process_index];
 let random_keyword_index = Math.floor(Math.random() * keywords.length);
 let random_keyword = keywords[random_keyword_index];
+let random_problem_index = Math.floor(Math.random() * problems.length);
+let random_problem = problems[random_problem_index];
 
 
 // Setup headline templates
 let formulas = [
     `How to ${random_process} in ${random_number} Simple Steps`,
-    `${random_keyword}: Everything You Need to Know`
+    `${random_keyword}: Everything You Need to Know`,
+    `${random_problem}? Here's How to Fix It`
 ];
-let random_formula_index = Math.floor(Math.random() * 2);
+let random_formula_index = Math.floor(Math.random() * formulas.length);
 let random_formula = formulas[random_formula_index];
 
 // Output random content idea
